092efd561598373eb16e84b4f6bbbddbb9b7947e
kimmisinha <kimmi.sinha@newput.com>
2024-08-21 16:25:02 +0530

    Configuring Dynamic Routes & Using Route Parameters

---
+++ app/blog/[slug]/page.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function BlogPost({params}) {
+  console.log(params)
+  return (
+    <div>BlogPost</div>
+  )
+}
+
+export default BlogPost
--- app/page.js
+++ app/page.js
@@ -5,7 +5,10 @@ export default function Home() {
     <main>
       <Header />
       <p>
+        <h2>Blog Post</h2>
         <Link href="/about">ABOUT PAGE</Link>
+        <Link href="/blog/post-1">Post_1</Link>
+        <Link href="/blog/post-2">Post_2</Link>
       </p>
     </main>
   );
