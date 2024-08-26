caee2ad47e2d2843bebed23616ec99cfb286d00f
kimmisinha <kimmi.sinha@newput.com>
2024-08-26 11:55:43 +0530

    Implemented data fetching with error handling and loading state managemen

--- app/(content)/news/page.js
+++ app/(content)/news/page.js
@@ -1,13 +1,43 @@
-import NewsList from '@/components/news-list';
-import { getAllNews } from '@/lib/news';
+'use client';
+import NewsList from "@/components/news-list";
+import { useEffect, useState } from "react";
 
-export default async function NewsPage() {
-  const news = await getAllNews();
+export default function NewsPage() {
+  const [isLoading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [news, setNews] = useState([]);
+
+  useEffect(() => {
+    async function fetchNews() {
+      setLoading(true);
+      try {
+        const response = await fetch("http://localhost:8080/news");
+        if (!response.ok) {
+          throw new Error('Failed to fetch news');
+        }
+        const data = await response.json();
+        setNews(data);
+      } catch (err) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    }
+    fetchNews();
+  }, []);
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>{error}</p>;
+  }
 
   return (
     <>
       <h1>News Page</h1>
-      <NewsList news={news} />
+      {news.length > 0 ? <NewsList news={news} /> : <p>No news available.</p>}
     </>
   );
 }
