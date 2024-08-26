5a99b994e15d0ad86d1485fea0500e963a481010
kimmisinha <kimmi.sinha@newput.com>
2024-08-26 17:26:34 +0530

    Controlling the Image Size

--- components/header.js
+++ components/header.js
@@ -1,18 +1,29 @@
 import Link from 'next/link';
+import Image from 'next/image';
+
+import logo from '@/assets/logo.png';
 
 export default function Header() {
   return (
     <header id="main-header">
-      <div id="logo">
-        <Link href="/">NextCaching</Link>
-      </div>
+      <Link href="/">
+        <Image
+          src={logo}
+          width={100}
+          height={100}
+          priority
+          alt="Mobile phone with posts feed on it"
+        />
+      </Link>
       <nav>
         <ul>
           <li>
-            <Link href="/messages">Messages</Link>
+            <Link href="/feed">Feed</Link>
           </li>
           <li>
-            <Link href="/messages/new">New Message</Link>
+            <Link className="cta-link" href="/new-post">
+              New Post
+            </Link>
           </li>
         </ul>
       </nav>
