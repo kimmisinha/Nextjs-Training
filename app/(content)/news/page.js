7c28620188a1d2e92395b500d8586297b1d956b6
kimmisinha <kimmi.sinha@newput.com>
2024-08-26 12:32:28 +0530

    Implement asynchronous news filtering with dynamic header and content

--- app/(content)/news/page.js
+++ app/(content)/news/page.js
@@ -1,43 +1,13 @@
-'use client';
-import NewsList from "@/components/news-list";
-import { useEffect, useState } from "react";
+import NewsList from '@/components/news-list';
+import { getAllNews } from '@/lib/news';
 
-export default function NewsPage() {
-  const [isLoading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
-  const [news, setNews] = useState([]);
-
-  useEffect(() => {
-    async function fetchNews() {
-      setLoading(true);
-      try {
-        const response = await fetch("http://localhost:8080/news");
-        if (!response.ok) {
-          throw new Error('Failed to fetch news');
-        }
-        const data = await response.json();
-        setNews(data);
-      } catch (err) {
-        setError(err.message);
-      } finally {
-        setLoading(false);
-      }
-    }
-    fetchNews();
-  }, []);
-
-  if (isLoading) {
-    return <p>Loading...</p>;
-  }
-
-  if (error) {
-    return <p>{error}</p>;
-  }
+export default async function NewsPage() {
+  const news = await getAllNews();
 
   return (
     <>
       <h1>News Page</h1>
-      {news.length > 0 ? <NewsList news={news} /> : <p>No news available.</p>}
+      <NewsList news={news} />
     </>
   );
 }
