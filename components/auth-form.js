34747ef4f0275e8b95b0fca2b59a655681d0c051
kimmisinha <kimmi.sinha@newput.com>
2024-08-27 12:03:34 +0530

    Update form to toggle between login and signup modes with conditional button text and links.

--- components/auth-form.js
+++ components/auth-form.js
@@ -26,7 +26,19 @@ export default function AuthForm({ mode }) {
           ))}
         </ul>
       )}
-     
+      <p>
+        <button type="submit">
+          {mode === 'login' ? 'Login' : 'Create Account'}
+        </button>
+      </p>
+      <p>
+        {mode === 'login' && (
+          <Link href="/?mode=signup">Create an account.</Link>
+        )}
+        {mode === 'signup' && (
+          <Link href="/?mode=login">Login with existing account.</Link>
+        )}
+      </p>
     </form>
   );
 }
